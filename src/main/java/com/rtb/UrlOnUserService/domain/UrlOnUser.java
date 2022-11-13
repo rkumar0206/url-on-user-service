ee3dbbc06b5fd530aebebb993fe9f9cce7c2429a
Rohit <rkumar8092378845@gmail.com>
2022-11-13 22:56:17 +0530

    - added endpoints for getting the self details and getting other user details using username
    #2

--- src/main/java/com/rtb/UrlOnUserService/domain/UrlOnUser.java
+++ src/main/java/com/rtb/UrlOnUserService/domain/UrlOnUser.java
@@ -1,5 +1,6 @@
 package com.rtb.UrlOnUserService.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import lombok.*;
 
 import javax.persistence.*;
@@ -19,27 +20,40 @@ public class UrlOnUser implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonIgnore
     private Long id;
 
     @Column(name = "email_id", nullable = false, unique = true, length = 50)
     private String emailId;
+
     @Column(name = "username", nullable = false, unique = true, length = 50)
     private String username;
+
+    @JsonIgnore
     @Column(name = "password", nullable = false, length = 100)
     private String password;
+
     @Column(name = "uid", nullable = false, unique = true, length = 50)
     private String uid;
 
     @Column(name = "first_name", nullable = false, length = 30)
     private String firstName;
+
     @Column(name = "last_name", length = 50)
     private String lastName;
+
     private String profileImage;
+
     private String phoneNumber;
+
     @Column(name = "dob", nullable = false)
     private Date dob;
+
+    @JsonIgnore
     @Column(name = "is_account_verified")
     private boolean isAccountVerified = false;
+
+    @JsonIgnore
     private String resetPasswordToken;
 
     @ManyToMany(fetch = FetchType.EAGER)
