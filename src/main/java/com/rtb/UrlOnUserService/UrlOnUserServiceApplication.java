2507c3b5dca2d24cb8d53ed39761dbea791ee31e
Rohit <rkumar8092378845@gmail.com>
2022-12-10 01:09:16 +0530

    - updated develop

--- src/main/java/com/rtb/UrlOnUserService/UrlOnUserServiceApplication.java
+++ src/main/java/com/rtb/UrlOnUserService/UrlOnUserServiceApplication.java
@@ -10,8 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-import java.util.ArrayList;
-import java.util.Date;
+import java.util.*;
 
 @SpringBootApplication
 @EnableEurekaClient
@@ -53,6 +52,32 @@ public class UrlOnUserServiceApplication {
             );
 
             userRepository.save(user);
+
+            List<String> userIds = Arrays.asList("rk0001", "rk0002", "rk0003", "rk0004", "rk0005");
+
+            userIds.forEach(uid -> {
+
+                int k = new Random().nextInt(100);
+
+                UserAccount userTemp = new UserAccount(
+                        null,
+                        "user" + k + "@gmail.com",
+                        Long.toHexString(new Random().nextLong()),
+                        getPasswordEncoder().encode("rohit"),
+                        uid,
+                        "User" + k,
+                        "lastName",
+                        null,
+                        null,
+                        new Date(),
+                        true,
+                        null,
+                        new ArrayList<>()
+                );
+
+                userRepository.save(userTemp);
+            });
+
         };
     }
 }
